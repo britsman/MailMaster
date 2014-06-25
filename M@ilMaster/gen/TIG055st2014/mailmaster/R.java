8d22d07ca5d59c4c9ccb68b2849fedef619f153a
khaled87 <khaled.nawasreh@gmail.com>
2014-06-25 19:26:08 +0200

    attach file

--- M@ilMaster/gen/TIG055st2014/mailmaster/R.java
+++ M@ilMaster/gen/TIG055st2014/mailmaster/R.java
@@ -26,10 +26,11 @@ public final class R {
     public static final class id {
         public static final int account_list=0x7f080004;
         public static final int account_text=0x7f080001;
-        public static final int action_settings=0x7f080014;
+        public static final int action_settings=0x7f080015;
+        public static final int attach_button=0x7f080012;
         public static final int body=0x7f080010;
         public static final int button=0x7f080008;
-        public static final int compose_button=0x7f080012;
+        public static final int compose_button=0x7f080013;
         public static final int delete_button=0x7f080000;
         public static final int email=0x7f080005;
         public static final int goto_add=0x7f080002;
@@ -42,7 +43,7 @@ public final class R {
         public static final int sendAcc=0x7f08000a;
         public static final int send_button=0x7f080011;
         public static final int sender=0x7f080009;
-        public static final int settings_button=0x7f080013;
+        public static final int settings_button=0x7f080014;
         public static final int subject=0x7f08000e;
         public static final int textView1=0x7f08000d;
     }
@@ -63,6 +64,7 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int add_acc=0x7f050006;
         public static final int app_name=0x7f050000;
+        public static final int attach=0x7f050012;
         public static final int compose=0x7f050011;
         public static final int email=0x7f050007;
         public static final int hello_world=0x7f050003;
--- M@ilMaster/src/TIG055st2014/mailmaster/ComposeActivity.java
+++ M@ilMaster/src/TIG055st2014/mailmaster/ComposeActivity.java
@@ -1,8 +1,20 @@
 package TIG055st2014.mailmaster;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -16,6 +28,12 @@ public class ComposeActivity extends Activity {
     private SharedPreferences accounts;
     private String defaultAcc;
     private String pw;
+    private static final int PICK_FROM_GALLERY = 101;
+    String  attachmentFile;
+    Uri URI = null;
+    int columnIndex;
+ //   private Multipart _multipart; 
+    
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +43,63 @@ public class ComposeActivity extends Activity {
         defaultAcc = accounts.getString("default", "");
         pw = accounts.getString(defaultAcc, "");
 	}
+	
 	@Override
 	protected void onStart() {
 		super.onStart();
 		TextView sender = (TextView) findViewById(R.id.sendAcc);
 		sender.setText(defaultAcc);
 	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ( requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
+               /**
+                * Get Path
+                */
+               Uri selectedImage = data.getData();
+               String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+               Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+               cursor.moveToFirst();
+               columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+               attachmentFile = cursor.getString(columnIndex);
+               Log.e("Attachment Path:", attachmentFile);
+	           URI = Uri.parse("file://" + attachmentFile);
+               cursor.close();
+               }
+        }
+	
+	
+	@SuppressLint("SdCardPath")
+	public void onClickAttach(View v) {
+		try{
+			/*BodyPart messageBodyPart = new MimeBodyPart(); 
+			String filelocation="downloadfile.bin";
+			DataSource source = new FileDataSource(filelocation); 
+			messageBodyPart.setDataHandler(new DataHandler(source)); 
+			messageBodyPart.setFileName(filelocation);
+			String body = ((EditText) findViewById(R.id.body)).getText().toString();
+		      messageBodyPart.setText(body); 
+		       _multipart.addBodyPart(messageBodyPart);*/
+		
+			openGallery();
+			
+		 }catch (Exception e) {   
+				Log.e("FileAttach", e.getMessage(), e);   
+			}
+		 }
+	
+	public void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data", true);
+        startActivityForResult(
+                     Intent.createChooser(intent, "Complete action using"),
+                     PICK_FROM_GALLERY);
+
+ }
+	
 	public void onClickSend(View v){
 		String recipients = ((EditText) findViewById(R.id.receiveAccs)).getText().toString();
 		String subject = ((EditText) findViewById(R.id.subject)).getText().toString();
