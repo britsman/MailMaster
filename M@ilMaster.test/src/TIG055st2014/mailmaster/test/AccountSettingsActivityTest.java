59fb6cb3094c7d34daef717dd588991c8d19e3c7
Britsman <eric_britsman@hotmail.com>
2014-07-08 14:43:18 +0200

    Added tests.

--- M@ilMaster.test/src/TIG055st2014/mailmaster/test/AccountSettingsActivityTest.java
+++ M@ilMaster.test/src/TIG055st2014/mailmaster/test/AccountSettingsActivityTest.java
@@ -1,21 +1,64 @@
 package TIG055st2014.mailmaster.test;
 
+import TIG055st2014.mailmaster.*;
 
+import TIG055st2014.mailmaster.R;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityUnitTestCase;
+import android.util.Log;
+import android.view.MenuItem;
 
+public class AccountSettingsActivityTest extends ActivityUnitTestCase<AccountSettingsActivity> {
 
-import android.test.InstrumentationTestCase;
-
-public class AccountSettingsActivityTest extends InstrumentationTestCase {
-
-	@Override
-	protected void setUp(){
+	private AccountSettingsActivity activity;
+	
+	public AccountSettingsActivityTest() {
+		super(AccountSettingsActivity.class);
 	}
 	@Override
+	protected void setUp() throws Exception{
+		super.setUp();
+		Intent intent = new Intent(getInstrumentation().getTargetContext(),
+		        AccountSettingsActivity.class);
+		startActivity(intent, null, null);
+		activity = getActivity();
+	}
+	/*@Override
 	protected void tearDown(){
-	}
+	}**/
 
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testChangeDefault() {
+		  activity.runOnUiThread(new Runnable() {
 
+		        public void run() {
+		        	String adress = "mailmastertesting@gmail.com";
+		        	activity.columns.add(adress);
+		        	activity.onItemClick(null, null, 0, 0);
+		        	assertTrue(activity.accounts.getString("default", "").equals(adress));
+		       }
+		    });
+	}
+	public void testToAdd() {
+		  activity.runOnUiThread(new Runnable() {
+
+		        public void run() {
+		        	String target = "TIG055st2014.mailmaster.AddAccountActivity";
+		        	MenuItem addIcon = (MenuItem)activity.findViewById(R.id.action_toAddAccount);
+		        	activity.toAdd(addIcon);
+		        	assertTrue(getStartedActivityIntent().getAction().equals(target));
+		       }
+		    });
+	}
+	public void testToInbox() {
+		  activity.runOnUiThread(new Runnable() {
+
+		        public void run() {
+		        	String target = "TIG055st2014.mailmaster.InboxActivity";
+		        	MenuItem inboxIcon = (MenuItem)activity.findViewById(R.id.action_toInbox);
+		        	activity.toInbox(inboxIcon);
+		        	assertTrue(getStartedActivityIntent().getAction().equals(target));
+		       }
+		    });
+	}
 }
--- M@ilMaster.test/src/TIG055st2014/mailmaster/test/AddAccountActivityTest.java
+++ M@ilMaster.test/src/TIG055st2014/mailmaster/test/AddAccountActivityTest.java
@@ -4,19 +4,99 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import android.test.InstrumentationTestCase;
+import TIG055st2014.mailmaster.AddAccountActivity;
+import TIG055st2014.mailmaster.R;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddAccountActivityTest extends InstrumentationTestCase {
+public class AddAccountActivityTest extends ActivityUnitTestCase<AddAccountActivity> {
 
-	@Override
-	protected void setUp(){
+	private AddAccountActivity activity;
+	
+	public AddAccountActivityTest() {
+		super(AddAccountActivity.class);
 	}
 	@Override
-	protected void tearDown(){
+	protected void setUp() throws Exception{
+		super.setUp();
+		Intent intent = new Intent(getInstrumentation().getTargetContext(),
+				AddAccountActivity.class);
+		startActivity(intent, null, null);
+		activity = getActivity();
 	}
 
-	public void test() {
-		fail("Not yet implemented");
+	public void testChangeDefault() {
+		  activity.runOnUiThread(new Runnable() {
+
+		        public void run() {
+		        	String adress = "mailmastertesting@gmail.com";
+		        	Button add = (Button) activity.findViewById(R.id.button);
+		            EditText email = ((EditText)activity.findViewById(R.id.email));
+		            EditText pw = ((EditText)activity.findViewById(R.id.password));
+		            email.setText(adress);
+		            pw.setText("mailmaster123");
+		            activity.accEdit.remove(adress);
+		            activity.accEdit.commit();
+		            int prevSize = activity.accounts.getAll().keySet().size();
+		        	add.performClick();
+		        	assertTrue(activity.accounts.getString("default", "").equals(adress) &&
+		        			   activity.accounts.getAll().keySet().size() == prevSize+1);
+		       }
+		    });
+	}
+	public void testAccountExists() {
+		  activity.runOnUiThread(new Runnable() {
+
+		        public void run() {
+		        	String adress = "mailmastertesting@gmail.com";
+		        	Button add = (Button) activity.findViewById(R.id.button);
+		            EditText email = ((EditText)activity.findViewById(R.id.email));
+		            EditText pw = ((EditText)activity.findViewById(R.id.password));
+		            email.setText(adress);
+		            pw.setText("mailmaster123");
+		            activity.accEdit.remove(adress);
+		            activity.accEdit.commit();
+		            int prevSize = activity.accounts.getAll().keySet().size();
+		        	add.performClick();
+		        	add.performClick();
+		        	assertTrue(activity.accounts.getString("default", "").equals(adress) &&
+		        			   activity.accounts.getAll().keySet().size() == prevSize+1);
+		       }
+		    });
+	}
+	public void testToSettingsViaAdd() {
+		  activity.runOnUiThread(new Runnable() {
+
+		        public void run() {
+		        	String target = "TIG055st2014.mailmaster.AccountSettingsActivity";
+		        	String adress = "mailmastertesting@gmail.com";
+		        	Button add = (Button) activity.findViewById(R.id.button);
+		            EditText email = ((EditText)activity.findViewById(R.id.email));
+		            EditText pw = ((EditText)activity.findViewById(R.id.password));
+		            email.setText(adress);
+		            pw.setText("mailmaster123");
+		            activity.accEdit.remove(adress);
+		            activity.accEdit.commit();
+		            int prevSize = activity.accounts.getAll().keySet().size();
+		        	add.performClick();
+		        	assertTrue(getStartedActivityIntent().getAction().equals(target));
+		       }
+		    });
+	}
+	public void testToSettingsViaIcon() {
+		  activity.runOnUiThread(new Runnable() {
+
+		        public void run() {
+		        	String target = "TIG055st2014.mailmaster.AccountSettingsActivity";
+		        	String adress = "mailmastertesting@gmail.com";
+		        	MenuItem settingsIcon = (MenuItem) activity.findViewById(R.id.action_settings);
+		        	activity.toSettings(settingsIcon);
+		        	assertTrue(getStartedActivityIntent().getAction().equals(target));
+		       }
+		    });
 	}
 
 }
