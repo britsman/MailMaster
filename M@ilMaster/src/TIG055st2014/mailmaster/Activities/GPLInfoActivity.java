61d2a959136bf04f9b346464ea5b49be1b9dd784
Eric Britsman <eric_britsman@hotmail.com>
2014-08-16 11:58:01 +0200

    added clickable link to full GPLv2 license description.

--- M@ilMaster/src/TIG055st2014/mailmaster/Activities/GPLInfoActivity.java
+++ M@ilMaster/src/TIG055st2014/mailmaster/Activities/GPLInfoActivity.java
@@ -5,9 +5,34 @@ import TIG055st2014.mailmaster.R.layout;
 import TIG055st2014.mailmaster.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.Menu;
 import android.widget.TextView;
 
+/* M@ilMaster Multi-Account Email Client
+Copyright (C) 2014 Eric Britsman & Khaled Alnawasreh
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+Version 2 only; as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Contact Info: eric_britsman@hotmail.com / khaled.nawasreh@gmail.com
+ */
+
+/**
+ * Activity used to display conditions/warranty in interface in accordance with GPLv2.
+ */
 public class GPLInfoActivity extends Activity {
 
 	@Override
@@ -20,27 +45,28 @@ public class GPLInfoActivity extends Activity {
 		super.onStart();
 		TextView tv = (TextView) findViewById(R.id.text_gpl_info);
 		if(getIntent().getStringExtra("type").equals("conditions")){
-			tv.setText("M@ilMaster Multi-Account Email Client\nCopyright (C) 2014 Eric Britsman & Khaled Alnawasreh\n" +
-		               "This program is free software; you can redistribute it and/or modify it under the terms of " +
-					   "the GNU General Public License Version 2 only; as published by the Free Software Foundation.\n" +
-		               "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; " +
-					   "without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. " +
-		               "See the GNU General Public License for more details.\nYou should have received a copy of " +
-					   "the GNU General Public License along with this program; if not, write to the Free Software " +
-					   "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.");
-}
+			tv.setText(Html.fromHtml("M@ilMaster Multi-Account Email Client<br><br>Copyright (C) 2014 Eric Britsman " +
+									 "& Khaled Alnawasreh<br><br>This program is free software; you can redistribute " +
+									 "it and/or modify it under the terms of the GNU General Public License Version " +
+									 "2 only; as published by the Free Software Foundation.<br><br>This program is " +
+									 "distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; " +
+									 "without even the implied warranty of MERCHANTABILITY or FITNESS FOR A " +
+									 "PARTICULAR PURPOSE. See the " + getResources().getString(R.string.gpl_link) + 
+									 " for more details."));
+			tv.setMovementMethod(LinkMovementMethod.getInstance());
+		}
 		else{
 			tv.setText("NO WARRANTY" + "\n\n" + "BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR THE PROGRAM," + 
-		               " TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER" + 
-					   " PARTIES PROVIDE THE PROGRAM \"AS IS\" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT " +
-		               "LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE " +
-					   "QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL " +
-		               "NECESSARY SERVICING, REPAIR OR CORRECTION.\n" + "IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING" +
-					   " WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE " +
-		               " LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE" +
-					   " OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES" +
-		               " SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER" + 
-					   " OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.");
+					" TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER" + 
+					" PARTIES PROVIDE THE PROGRAM \"AS IS\" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT " +
+					"LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE " +
+					"QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL " +
+					"NECESSARY SERVICING, REPAIR OR CORRECTION.\n\n" + "IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING" +
+					" WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE " +
+					" LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE" +
+					" OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES" +
+					" SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER" + 
+					" OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.");
 		}
 	}
 	@Override
